0fb93484e85a9b6cbcd789cce7002260ce75d231
HoHow <wumingho@gmail.com>
2025-08-13 20:32:22 +0800

    add action text function

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,7 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
 
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
